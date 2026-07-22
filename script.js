f73a837ee41896d1b12ca7fd63781534d5063560
Pedrobr57040 <pedrobr57040@gmail.com>
2026-07-21 22:37:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function entrar() {
+  location.href = "home.html";
+}
+
+function sair() {
+  location.href = "index.html";
+}
+
+function consultas() {
+window.location.href = "consultas.html"
+}
+
+function agendarconsulta(){
+  alert("Consulta agendada com sucesso");
+}
+
+function voltar() {
+  window.location.href = "home.html"
+}
+
+function historico() {
+window.location.href = "historico.html"
+}
+
+function exames() {
+window.location.href = "exames.html";
+}
+
+function agendarExame() {
+alert("Exame agendado com sucesso");
+}
+
+function vacinas() {
+    window.location.href = "vacinas.html";
+}
+
+function voltar() {
+    window.location.href = "home.html";
+}
